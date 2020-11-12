a87d1404b84d97dc2e961335782d10cea00feb45
Nhat Cuong <nhatcuongboy@gmail.com>
2020-11-12 18:34:21 +0700

    fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Hello Cuong ss SSS22ss3333ss
+          Hello Word
         </p>
         <a
           className="App-link"
